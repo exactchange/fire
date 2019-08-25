906f06cd0311d0152108d67edec4bcaa1c3b021f
hello@bennyschmidt.com <hello@bennyschmidt.com>
2019-08-24 20:43:18 -0700

    add "npm restart", move didLoad to library

--- examples/saas-backend/start.js
+++ examples/saas-backend/start.js
@@ -7,5 +7,5 @@ const { Node } = require('./node');
 
 const node = new Node();
 
-ƒ.root.args.forEach(a => node.setState({ [a.split('=')[0]]: a.split('=')[1] }));
-node.didLoad();
+node.setShape({ dbVersion: ƒ.Type.String });
+ƒ.root.args.forEach(async a => await node.setState({ [a.split('=')[0]]: a.split('=')[1] }));
